c7e1efe0c6830f3af37b975297c6e90bff3aee38
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-03-05 17:45:45 +0100

    mod

--- src/es1/Esercizio1.java
+++ src/es1/Esercizio1.java
@@ -9,37 +9,35 @@ public class Esercizio1 {
         String inputString = scanner.nextLine();
         if (isEven(inputString.length())) {
             System.out.println("Il numero dei caratteri della Stringa è dispari");
-        }else{
+        } else {
             System.out.println("Il numero dei caratteri della Stringa è pari");
         }
 
         Scanner scanner1 = new Scanner(System.in);
         System.out.println("Inserisci un anno:");
-        int inputyear = scanner.nextInt();
+        int inputyear = scanner1.nextInt();
         if (isBisestile(inputyear)) {
             System.out.println("L'anno inserito è bisestile");
-        }else{
+        } else {
             System.out.println("L'anno inserito non è bisestile");
         }
-}
-/* primo metodo */
-    public static Boolean isEven(int num) {
-            if (num % 2 == 0) {
-                return false;
-            }else{
-                return true;
-            }
     }
-    /*secondo metodo*/
+
+    /* primo metodo */
+    public static Boolean isEven(int num) {
+        if (num % 2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /* secondo metodo */
     public static Boolean isBisestile(int num) {
         if (num % 4 == 0 || (num % 100 == 0 && num % 400 == 0)) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    }
-
-
-
-
+}
